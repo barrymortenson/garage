7872bdbc75c5095a3e0547280db0f2765a3c8589
Barry Mortenson <bmortenson@gmail.com>
2017-02-25 08:41:01 -0800

    big buttons

--- resources/assets/js/components/Lift.vue
+++ resources/assets/js/components/Lift.vue
@@ -1,27 +1,47 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-11 col-lg-offset-1">
+            <div class="col-sm-5">
                 <br>
-                <button type="button" @mousedown="up" @touchstart="touch" @mouseup="stop" @mouseleave="stop" class="btn btn-success btn-lg"><i class="fa fa-arrow-up fa-5x"></i></button>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-11">
-                <br>
-                <button type="button" @mousedown="down" @mouseup="stop" @mouseleave="stop" class="btn btn-danger btn-lg"><i class="fa fa-arrow-down fa-5x"></i></button>
-            </div>
-        </div>
+                <button type="button"
+                    @mousedown="up"
+                    @mouseup="stop"
+                    @mouseleave="stop"
+                    class="btn btn-success"
+                >
+                    <i class="fa fa-arrow-up" style="font-size: 196px;"></i>
+                </button>
 
-        <div class="row">
-            <br><br>
-            {{motion}}
-            {{touchme}}
-        </div>
+                <br><br>
 
-        <div class="row">
-            <br>
-            <button type="button" @click="reload" class="btn btn-default btn-lg">Reload</button>
+                <button type="button"
+                    @mousedown="down"
+                    @mouseup="stop"
+                    @mouseleave="stop"
+                    class="btn btn-danger"
+                >
+                    <i class="fa fa-arrow-up" style="font-size: 196px;"></i>
+                </button>
+            </div>
+
+            <div class="col-sm-2">
+                <table>
+                    <tr>
+                        <td style="vertical-align: middle">
+                            <button type="button" class="btn btn-default">
+                                &nbsp;<i class="fa fa-lock" style="font-size: 180px;"></i>&nbsp;
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+                <br><br>
+                {{motion}}
+                {{touchme}}
+            </div>
+
+            <div class="col-sm-5 text-right">
+                <button type="button" @click="reload" class="btn btn-default btn-lg">Reload</button>
+            </div>
         </div>
     </div>
 </template>
