817627a9d9197a71ab459c780fa8e8d7f1df6d29
Barry Mortenson <bmortenson@gmail.com>
2017-07-21 12:32:27 -0700

    use common settings file

--- relay_scripts/setup.py
+++ relay_scripts/setup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+import settings
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-# all gpio
+# set all gpio to off
 pins = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 GPIO.setup(pins, GPIO.OUT, initial=GPIO.HIGH)
